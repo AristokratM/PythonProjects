ffc64b94f2183bbd0df94ea4d222382f991aea6c
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-10-07 20:46:27 +0300

    asyn version

---
+++ async_8.py
@@ -0,0 +1,66 @@
+import os
+import requests
+from time import time
+
+
+def get_file(url):
+    r = requests.get(url, allow_redirects=True)
+    return r
+
+
+def write_file(response):
+    filename = response.url.split('/')[-1]
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    t0 = time()
+
+    url = "https://loremflickr.com/320/240"
+    os.chdir("./images")
+    for i in range(10):
+        write_file(get_file(url))
+
+    print(time() - t0)
+
+
+# if __name__ == '__main__':
+#     main()
+
+######################
+
+
+import asyncio
+import aiohttp
+
+
+def write_image(data):
+    filename = "file-{}.jpeg".format(int(time() * 1000))
+    with open(filename, "wb") as file:
+        file.write(data)
+
+
+async def fetch_data(url, session):
+    async with session.get(url, allow_redirects=True) as response:
+        data = await response.read()
+        write_image(data)
+
+
+async def main1():
+
+    url = "https://loremflickr.com/320/240"
+    tasks = []
+    os.chdir("./images")
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            task = asyncio.create_task(fetch_data(url, session))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    to = time()
+    asyncio.run(main1())
+    print(time() - to)
